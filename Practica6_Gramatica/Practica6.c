2442f9cdddf475315b3acba4690a27ba5d864877
Miguel Gonzalez <mikigonza52@gmail.com>
2022-03-11 23:20:25 -0600

    Practica 6 Gramatica

--- Practica6_Gramatica/Practica6.c
+++ Practica6_Gramatica/Practica6.c
@@ -18,23 +18,29 @@ int len(String cadena){
 void mensajeError(){
     printf("La cadena ingresada no pertenece a la gramatica\n");
 }
+//Imprime mensaje de exito
 void mensajeExito(){
     printf("La cadena pertenece a la gramatica\n");
 }
+//Regla de produccion A, recibe como parametro un inicio
+//El cual indicara la posición donde se empezará a recorrer la cadena 
+//que entra como parametro a la funcion
 void reglaProduccionA(int inicio, String expresion){
-    printf("%c ", *(expresion+inicio));
+    //El caso base se presenta si la posiciopon inicial de la cadena
+    //e igual al caracter nulo, entonces significa que se ha estado
+    //recorriendo la cadena sin problemas y ha llegado al final, por lo tanto imprime mensaje de exito
     if (*(expresion+inicio) == '\0') mensajeExito();
+    //Si la cadena tiene un caracter'a', entonces se mandará a llamar la regla de producción E una posición, adelante de la actual
     else if(*(expresion+inicio) == 'a') reglaProduccionE(inicio+1, expresion);
+    //En caso de que no sea un caracter perteneciente al lenguaje, esta rompera el bucle
     else mensajeError();
 }
 void reglaProduccionB(int inicio, String expresion){
-    printf("%c ", *(expresion+inicio));
     if (*(expresion+inicio) == '\0') mensajeExito();
     else if(*(expresion+inicio) == 'b') reglaProduccionE(inicio+1, expresion);
     else mensajeError();
 }
 void reglaProduccionE(int inicio, String expresion){
-    printf("%c ", *(expresion+inicio));
     if(*(expresion+inicio) == '\0') mensajeExito();
     else if(*(expresion+inicio) == 'a') reglaProduccionB(inicio+1, expresion);
     else if(*(expresion+inicio) == 'b') reglaProduccionA(inicio+1, expresion);
@@ -43,21 +49,26 @@ void reglaProduccionE(int inicio, String expresion){
 }
 
 int main(){
-    //Limpiamos el buffer de entrada de datos
-    fflush(stdin);
-    //Reservados espacio de memoria para una cadena de maximo 100 caracteres
-    String cad = malloc(100*sizeof(100));
+    int opc = 1;
     printf("***Evaluador de gramatica***\n");
-    printf("Definida por las siguientes reglas de produccion:\n1.E -> aB | bA | E\n2.A -> aE\n3.B -> bE\n******\n\n");
-    printf("Ingrese la cadena a evaluar:");
-    //Leemos cadena
-    scanf("%[^\n]", cad);
-    //Redimensionamos la memoria de la cadena por una de la longitud ingredada
-    cad = realloc(cad, len(cad)+1);
-    //Iniciamos la evaluacion con la regla de produccion 1
-    reglaProduccionE(0,cad);
-    //Liberamos la cadena de la memoria del sistema
-    cad = NULL;
-    free(cad);
+    printf("Definida por las siguientes reglas de produccion:\n1.E -> aB | bA | E\n2.A -> aE\n3.B -> bE\n");
+    do{
+        //Limpiamos el buffer de entrada de datos
+        fflush(stdin);
+        //Reservados espacio de memoria para una cadena de maximo 100 caracteres
+        String cad = malloc(100*sizeof(char));
+        printf("Ingrese la cadena a evaluar:");
+        //Leemos cadena
+        scanf("%[^\n]", cad);
+        //Redimensionamos la memoria de la cadena por una de la longitud ingredada
+        cad = realloc(cad, len(cad)+1);
+        //Iniciamos la evaluacion con la regla de produccion 1
+        reglaProduccionE(0,cad);
+        //Liberamos la cadena de la memoria del sistema
+        cad = NULL;
+        free(cad);
+        printf("\nDesea realizar otra operacion?\nPulse:\n0.No 1.Si\nIngrese opcion:");
+        scanf("%d", &opc);
+    }while(opc != 0);
     return 0;
 }
